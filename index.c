c748c9aa6dee9f1603aa6ae35fa7f14e239ad7b3
Tofayel Hyder <abhihyder7@gmail.com>
2022-12-09 19:28:58 +0600

    target and history

--- index.c
+++ index.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_EMPLOYEE 5
 #define EMPLOYEE_FILE "data/employee.txt"
 #define SALES_TARGET_FILE "data/sales_target.txt"
 #define SALES_HISTORY_FILE "data/sales_history.txt"
-#define BEST_EMPLOYEE_FILE "data/sales_history.txt"
 
 typedef struct
 {
@@ -18,12 +16,27 @@ typedef struct
 
 Employee employee;
 
+typedef struct
+{
+    char employee_id[10];
+    char month[10];
+    int amount;
+} SalesTarget;
+
+SalesTarget sales_target;
+
+typedef struct
+{
+    char employee_id[10];
+    char month[10];
+    int amount;
+} SalesHistory;
+
+SalesHistory sales_history;
+
 int main()
 {
-    // generateEmployee();
-
     menu();
-
     return 0;
 }
 
@@ -44,11 +57,11 @@ void menu()
             displayEmployee();
             break;
         case '2':
-
+            displaySalesTarget();
             break;
 
         case '3':
-
+            displaySalesHistory();
             break;
         case '4':
 
@@ -83,6 +96,46 @@ void generateEmployee()
     fprintf(file, "%s\t\t\t%d\t\t%s\t\t%s\t\t%d\n", employee.name, employee.age, employee.employee_id, employee.gender, employee.salary);
     fclose(file);
     displayEmployee();
+}
+
+void generateTarget()
+{
+    FILE *file;
+    file = fopen(SALES_TARGET_FILE, "a");
+    if (file == NULL)
+    {
+        printf("Something went wrong!\n");
+        return;
+    }
+    printf("Enter Employee ID: ");
+    scanf("%s", sales_target.employee_id);
+    printf("\nEnter Target Month: ");
+    scanf("%s", sales_target.month);
+    printf("\nEnter Target Amount: ");
+    scanf("%d", &sales_target.amount);
+    fprintf(file, "%s\t\t%s\t\t%d\n", sales_target.employee_id, sales_target.month, sales_target.amount);
+    fclose(file);
+    displaySalesTarget();
+}
+
+void generateHistory()
+{
+    FILE *file;
+    file = fopen(SALES_HISTORY_FILE, "a");
+    if (file == NULL)
+    {
+        printf("Something went wrong!\n");
+        return;
+    }
+    printf("Enter Employee ID: ");
+    scanf("%s", sales_history.employee_id);
+    printf("\nEnter History Month: ");
+    scanf("%s", sales_history.month);
+    printf("\nEnter History Amount: ");
+    scanf("%d", &sales_history.amount);
+    fprintf(file, "%s\t\t%s\t\t%d\n", sales_history.employee_id, sales_history.month, sales_history.amount);
+    fclose(file);
+    displaySalesHistory();
 }
 
 void displayEmployee()
@@ -124,8 +177,94 @@ void displayEmployee()
         break;
     default:
         printf("\n\n\t\t---------- Wrong Choice -----------\n\n");
+        displayEmployee();
     }
 }
+
+void displaySalesTarget()
+{
+    FILE *file;
+    file = fopen(SALES_TARGET_FILE, "r");
+
+    if (file == NULL)
+    {
+        printf("Error in reading or does not exist the file '%s'", SALES_TARGET_FILE);
+        return;
+    }
+    else
+    {
+        tableStart("SALES TARGET LIST START");
+        printf("\nEMPLOYEE_ID\t\tMONTH\t\tAMOUNT\n");
+        border();
+        while (fscanf(file, "%s\t\t%s\t\t%d\n", sales_target.employee_id, sales_target.month, &sales_target.amount) != EOF)
+        {
+            printf("\n%s\t\t\t%s\t\t%d\n", sales_target.employee_id, sales_target.month, sales_target.amount);
+            border();
+        }
+        tableStart("SALES TARGET LIST END");
+    }
+    printf("\n\n1. Add Target\n2. Back To Main Menu\nYour Choice: ");
+    char choice;
+    choice = getche();
+
+    system("cls");
+
+    switch (choice)
+    {
+    case '1':
+        generateTarget();
+        break;
+    case '2':
+        menu();
+        break;
+    default:
+        printf("\n\n\t\t---------- Wrong Choice -----------\n\n");
+        displaySalesTarget();
+    }
+}
+
+void displaySalesHistory()
+{
+    FILE *file;
+    file = fopen(SALES_HISTORY_FILE, "r");
+
+    if (file == NULL)
+    {
+        printf("Error in reading or does not exist the file '%s'", SALES_HISTORY_FILE);
+        return;
+    }
+    else
+    {
+        tableStart("SALES HISTORY LIST START");
+        printf("\nEMPLOYEE_ID\t\tMONTH\t\tAMOUNT\n");
+        border();
+        while (fscanf(file, "%s\t\t%s\t\t%d\n", sales_history.employee_id, sales_history.month, &sales_history.amount) != EOF)
+        {
+            printf("\n%s\t\t\t%s\t\t%d\n", sales_history.employee_id, sales_history.month, sales_history.amount);
+            border();
+        }
+        tableStart("SALES HISTORY LIST END");
+    }
+    printf("\n\n1. Add History\n2. Back To Main Menu\nYour Choice: ");
+    char choice;
+    choice = getche();
+
+    system("cls");
+
+    switch (choice)
+    {
+    case '1':
+        generateHistory();
+        break;
+    case '2':
+        menu();
+        break;
+    default:
+        printf("\n\n\t\t---------- Wrong Choice -----------\n\n");
+        displaySalesHistory();
+    }
+}
+
 void border()
 {
     printf("-------------------------------------------------------------------------------");
